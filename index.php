e52fbfd0a6fbdf1b845609600990ecd9923a8022
Moises Suquila <moisessuquila@wenykeny.home>
2020-03-02 10:13:31 +0200

    Update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Mortgage Calculator</title>
+    
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
